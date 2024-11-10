3b89501748d21ce40b87c81449c50b18f3c9ae62
cloud9ight <cloud9ight@gmail.com>
2024-11-11 01:20:50 +0800

    tailwindcss config

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Store Here - Where you can store everything.</h1>
+      <h1 className="text-3xl">Store Here - Where you can store everything.</h1>
     </div>
   );
 }
