58217dcc6a647dcb71a27d5502eb351956f630c7
cloud9ight <cloud9ight@gmail.com>
2025-03-11 15:29:16 +0800

    update global css for style guide

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl">Store Here - Where you can store everything.</h1>
+    <div className="flex-center h-screen">
+      <h1 className="h1">StoreHere - The storage solution master you need.</h1>
     </div>
   );
 }
