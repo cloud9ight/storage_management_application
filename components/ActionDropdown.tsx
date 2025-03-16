f89d115c42e04c61f9c11b2c63ed5b6a85677992
cloud9ight <cloud9ight@gmail.com>
2025-03-16 11:15:27 +0800

    created dropdown menu in card. updated db permission.

---
+++ components/ActionDropdown.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import React, { useState } from "react";
+import { Dialog } from "@/components/ui/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Image from "next/image";
+import { Models } from "node-appwrite";
+
+const ActionDropdown = ({ file }: { file: Models.Document }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  return (
+    <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
+      <DropdownMenu open={isDropdownOpen} onOpenChange={setIsDropdownOpen}>
+        <DropdownMenuTrigger className="shad-no-focus">
+          <Image
+            src="/assets/icons/dots.svg"
+            alt="dots"
+            width={34}
+            height={34}
+          />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel className="max-w-[200px] truncate">
+            {file.name}
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>rename</DropdownMenuItem>
+          <DropdownMenuItem>share</DropdownMenuItem>
+          <DropdownMenuItem>delete</DropdownMenuItem>
+          <DropdownMenuItem>details</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </Dialog>
+  );
+};
+
+export default ActionDropdown;
