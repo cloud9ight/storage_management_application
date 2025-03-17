74e226b71239f5cade03406f58684f63ce6630b2
cloud9ight <cloud9ight@gmail.com>
2025-03-17 13:48:57 +0800

    updated used space functionality. update dashboard ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,30 @@
 import Image from "next/image";
+import { getFiles, getTotalSpaceUsed } from "@/lib/actions/file.actions";
+import { convertFileSize, getUsageSummary } from "@/lib/utils";
 
-export default function Home() {
+const Dashboard = async () => {
+  // Parallel requests
+  const [files, totalSpace] = await Promise.all([
+    getFiles({ types: [], limit: 10 }),
+    getTotalSpaceUsed(),
+  ]);
+
+  // Get usage summary
+  const usageSummary = getUsageSummary(totalSpace);
+
   return (
-    <div className="flex-center h-screen">
-      <h1 className="h1">StoreHere - The storage solution master you need.</h1>
+    <div className="dashboard-container">
+      <section>
+        <h2 className="h3 xl:h2 text-light-100">
+          Uploaded file type summaries
+        </h2>
+        {/* Display the result in a preformatted block */}
+        <pre>{totalSpace.used}</pre>
+      </section>
+      <section className="dashboard-recent-files">
+        <h2 className="h3 xl:h2 text-light-100">Recent files uploaded</h2>
+      </section>
     </div>
   );
-}
+};
+export default Dashboard;
