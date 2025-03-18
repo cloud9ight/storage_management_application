90b332126b417e2fddff102f7b02db4cf3cd40a9
cloud9ight <cloud9ight@gmail.com>
2025-03-18 09:34:02 +0800

    update dashboard to show each file category storage and link together

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 import { getFiles, getTotalSpaceUsed } from "@/lib/actions/file.actions";
 import { convertFileSize, getUsageSummary } from "@/lib/utils";
 import { Chart } from "@/components/Chart";
+import Link from "next/link";
+import { Separator } from "@/components/ui/separator";
+import FormattedDateTime from "@/components/FormattedDateTime";
 
 const Dashboard = async () => {
   // Parallel requests
@@ -17,6 +20,38 @@ const Dashboard = async () => {
     <div className="dashboard-container">
       <section>
         <Chart used={totalSpace.used} />
+        {/* Uploaded file type summaries */}
+        <ul className="dashboard-summary-list">
+          {usageSummary.map((summary) => (
+            <Link
+              href={summary.url}
+              key={summary.title}
+              className="dashboard-summary-card"
+            >
+              <div className="space-y-4">
+                <div className="flex justify-between gap-3">
+                  <Image
+                    src={summary.icon}
+                    width={100}
+                    height={100}
+                    alt="uploaded image"
+                    className="summary-type-icon"
+                  />
+                  <h4 className="summary-type-size">
+                    {convertFileSize(summary.size) || 0}
+                  </h4>
+                </div>
+
+                <h5 className="summary-type-title">{summary.title}</h5>
+                <Separator className="bg-light-400" />
+                <FormattedDateTime
+                  date={summary.latestDate}
+                  className="text-center"
+                />
+              </div>
+            </Link>
+          ))}
+        </ul>
 
         {/* Uploaded file type summaries */}
       </section>
